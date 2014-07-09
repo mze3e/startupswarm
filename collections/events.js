7530f7cb5a366f798797deea27e82f6aae3bfe6d
Ahmed Muzammil <ahmedmzl@gmail.com>
2014-07-09 23:35:44 +0800

    before clearing the data

--- collections/events.js
+++ collections/events.js
@@ -45,15 +45,15 @@ Events = new Meteor.Collection("events", {
 Events.allow({
   insert: function (userId, doc) {
     // the user must be logged in, and the document must be owned by the user
-    return (userId && doc.owner === userId);
+    return isAdmin(userId);
   },
   update: function (userId, doc, fields, modifier) {
     // can only change your own documents
-    return doc.owner === userId;
+    return isAdmin(userId);
   },
   remove: function (userId, doc) {
     // can only remove your own documents
-    return doc.owner === userId;
+    return isAdmin(userId);
   },
   fetch: ['owner']
 });
@@ -65,7 +65,7 @@ Events.deny({
   },
   remove: function (userId, doc) {
     // can't remove locked documents
-    return doc.locked;
+    return doc.locked;f
   },
   fetch: ['locked'] // no need to fetch 'owner'
 });
